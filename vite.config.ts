38d8e252486d9dfe8309018b1aaebf35eb2c4e96
hksaham <hksaham57@gmail.com>
2026-04-03 19:12:03 +0600

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 export default defineConfig({
   // 1. Set the base path for GitHub Pages (e.g., '/bikes-bazar/')
   // If your repo is "bikes-bazar-showcase-main", use that here:
-  base: "/BIKES-BAZAR/", 
+  base: "/bikes-bazar-web/", 
 
   server: {
     host: "0.0.0.0",
